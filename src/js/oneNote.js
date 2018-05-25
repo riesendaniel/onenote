7fd4bcac6042c0ebd8d57739231c4262e6c9b32f
Daniel Riesen <noreply@riesen.ch>
2018-05-25 22:02:10 +0200

    erste Version

--- src/js/oneNote.js
+++ src/js/oneNote.js
@@ -1,45 +1,34 @@
 var color = sessionStorage.getItem("color");
-if( !color )
-{
+if (!color) {
     sessionStorage.setItem("color", JSON.stringify(["white"]));
     color = sessionStorage.getItem("color");
 }
 color = JSON.parse(color);
 document.body.style.backgroundColor = color;
 
-//var notes = sessionStorage.getItem("notes");
-//if( !notes )
-//{
-//    sessionStorage.setItem("notes", JSON.stringify([]));
-//    notes = sessionStorage.getItem("notes");
-//}
-//notes = JSON.parse(notes);
-
-const notes = [
-    {"id":"01", "title":"Notiz 1", "status": "offen", "beschreibung":"Abfall entsorgen", "rating":1, "erledigtbis":"2018-12-01"},
-    {"id":"02", "title":"Notiz 2", "status": "erledigt", "beschreibung":"Projekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellen", "rating":1, "erledigtbis":"2018-06-28"},
-    {"id":"03", "title":"Notiz 3", "status": "offen", "beschreibung":"Dies und das", "rating":3, "erledigtbis":"2019-01-01"},
-    {"id":"01", "title":"Notiz 1", "status": "erledigt", "beschreibung":"Abfall entsorgen", "rating":1, "erledigtbis":"2018-12-01"},
-    {"id":"02", "title":"Notiz 2", "status": "erledigt", "beschreibung":"Projekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellenProjekt Schule fertig stellen", "rating":1, "erledigtbis":"2018-06-28"},
-    {"id":"03", "title":"Notiz 3", "status": "offen", "beschreibung":"Dies und das", "rating":3, "erledigtbis":"2019-01-01"}
-];
+var notes = sessionStorage.getItem("notes");
+if (!notes) {
+    sessionStorage.setItem("notes", JSON.stringify([]));
+    notes = sessionStorage.getItem("notes");
+}
+notes = JSON.parse(notes);
 
 function changeColor(form_element) {
     sessionStorage.setItem("color", JSON.stringify(form_element.value));
     document.body.style.backgroundColor = form_element.value;
 }
 
-function createNotesList (notes) {
-    let htmlString ="";
+function createNotesList(notes) {
+    let htmlString = "";
     notes.forEach(function (note) {
         htmlString += "<li>";
         htmlString += "<a href=\"createNote.html\">";
         htmlString += "<h2>";
         htmlString += note.title;
-        htmlString+= !note.rating ? "" : " " + "*".repeat(note.rating);
+        htmlString += !note.rating ? "" : " " + "*".repeat(note.rating);
         htmlString += "</h2>";
         htmlString += "<p>";
-        htmlString += note.beschreibung;
+        htmlString += note.description;
         htmlString += "</p>";
         htmlString += "<input id=\"checkBox\" type=\"checkbox\">";
         htmlString += " finished";
@@ -49,21 +38,40 @@ function createNotesList (notes) {
     return htmlString;
 }
 
-function sendButtonClickEvent(event) {
-    event.target.dataset.title;
-    const notes = JSON.parse(sessionStorage.getItem("notes"));
-    notes.push(document.getElementById("title").value);
-    sessionStorage.setItem("notes", JSON.stringify(notes));
-    window.location.replace("list.html");
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
 }
 
-function renderSongs () {
-    if(document.getElementById("notes")){
-        document.getElementById("notes").innerHTML=createNotesList(notes);
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
+
+function sendButtonClickEvent(event){
+    if(document.getElementById("title").checkValidity() && document.getElementById("description").checkValidity()) {
+        var json = JSON.parse('{"id":"' + guid() + '", "title":"' + document.getElementById("title").value + '", "status": "offen", "description":"'
+            + document.getElementById("description").value + '", "rating":1, "erledigtbis":"2018-12-01"}');
+        notes.push(json);
+        sessionStorage.setItem("notes", JSON.stringify(notes));
+        window.location.replace("index.html");
     }
 }
 
-window.onload = function() {
+function renderSongs() {
+    if (document.getElementById("notes")) {
+        document.getElementById("notes").innerHTML = createNotesList(notes);
+    }
+}
+
+function submitButton() {
+    if (document.getElementById("submit")) {
+        document.getElementById("submit").addEventListener("click", sendButtonClickEvent);
+    }
+}
+
+window.onload = function () {
     renderSongs();
-    document.getElementById("submit").addEventListener("click", sendButtonClickEvent);
+    submitButton();
 }
