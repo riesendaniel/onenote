06533786e31f17d60303c5a04d8fc2d10b3e6676
Daniel <riesen@yahoo.de>
2018-06-12 22:37:48 +0200

    refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -12,48 +12,69 @@ function changeColor(form_element) {
     document.body.style.backgroundColor = form_element.value;
 }
 
+
 function createNotesList(notes) {
     let htmlString = "";
     notes.forEach(function (note) {
-        htmlString += "<li>";
-        htmlString += "<a href=\"createNote.html\">";
-        htmlString += "<h2>";
-        htmlString += note.title;
-        htmlString += !note.rating ? "" : " " + "*".repeat(note.rating);
-        htmlString += "</h2>";
-        htmlString += "<p>";
-        htmlString += note.description;
-        htmlString += "</p>";
-        htmlString += "<input id=\"checkBox\" onchange='changeStatus(\"" + note.id + "\")' type=\"checkbox\">";
-        htmlString += " finished";
-        htmlString += "</input>";
-        htmlString += "</li>";
-    });
+            htmlString += "<li>";
+            if (note.status === "offen") {
+                htmlString += "<a onclick='editNote(\"" + note.id + "\")'>";
+            } else {
+                htmlString += "<a>";
+            }
+            htmlString += "<h2>";
+            htmlString += note.title;
+            htmlString += !note.rating ? "" : " " + "*".repeat(note.rating);
+            htmlString += "</h2>";
+            htmlString += "<p>";
+            htmlString += note.description;
+            htmlString += "</p>";
+            if (note.status === "offen") {
+                htmlString += "<input id=\"checkBox\"  onchange='changeStatus(\"" + note.id + "\")' type=\"checkbox\">";
+            }
+            else {
+                htmlString += "<input id=\"checkBox\" checked onchange='changeStatus(\"" + note.id + "\")' type=\"checkbox\">";
+            }
+            htmlString += " finished ( " + note.erledigtbis + " )";
+            htmlString += "</input>";
+            htmlString += "</li>";
+        }
+    )
+    ;
     return htmlString;
 }
-function changeStatus(id){
+
+function getURLParameter(name) {
+    var value = decodeURIComponent((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, ""])[1]);
+    return (value !== 'null') ? value : false;
+}
+
+function editNote(id) {
+    window.location.replace("createNote.html?id=" + id);
+}
+
+function changeStatus(id) {
     const note = getNoteById(id);
+    updateStatus(note);
 }
 
+function sendButtonClickEvent(event) {
+    const id = document.getElementById("noteID").value;
 
-function guid() {
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
+    const star = document.getElementById("star1").checked ? 1 : document.getElementById("star2").checked ? 2 : document.getElementById("star3").checked ? 3 : document.getElementById("star4").checked ? 4 : document.getElementById("star5").checked ? 5 : 0;
 
-function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-}
-
-function sendButtonClickEvent(event){
-    if(document.getElementById("title").checkValidity() && document.getElementById("description").checkValidity()) {
-        var note = JSON.parse('{"id":"' + guid() + '", "title":"' + document.getElementById("title").value + '", "status": "offen", "description":"'
-            + document.getElementById("description").value + '", "rating":1, "erledigtbis":"2018-12-01"}');
-        addNote(note)
+    if (id && document.getElementById("title").checkValidity() && document.getElementById("description").checkValidity() && document.getElementById("datePicker").checkValidity()) {
+        updateNoteData(id, document.getElementById("title").value, document.getElementById("description").value, star, document.getElementById("datePicker").value)
+    } else if (document.getElementById("title").checkValidity() && document.getElementById("description").checkValidity() && document.getElementById("datePicker").checkValidity()) {
+        addNote(document.getElementById("title").value, "offen", document.getElementById("description").value, star, document.getElementById("datePicker").value);
         window.location.replace("index.html");
     }
+}
+
+function sortFilter(event) {
+    const sort = document.getElementById("finishDate").checked ? 1 : document.getElementById("createDate").checked ? 2 : 3;
+    const showFinished = document.getElementById("showFinished").checked;
+    document.getElementById("notes").innerHTML = createNotesList(getNotes(sort, showFinished));
 }
 
 function renderNotes() {
@@ -68,7 +89,25 @@ function submitButton() {
     }
 }
 
+function initSortFilter() {
+    if (document.getElementById("sort") && document.getElementById("showFinished")) {
+        document.getElementById("sort").addEventListener("click", sortFilter);
+        document.getElementById("showFinished").addEventListener("click", sortFilter);
+    }
+}
+
 window.onload = function () {
     renderNotes();
     submitButton();
+    initSortFilter();
+
+    var urlId = getURLParameter('id');
+    if (urlId) {
+        const note = getNoteById(urlId);
+        document.getElementById("title").value = note.title;
+        document.getElementById("description").value = note.description;
+        document.getElementById("urgency").value = note.erledigtbis;
+        document.getElementById("noteID").value = note.id;
+
+    }
 }
--- src/js/notesStorage.js
+++ src/js/notesStorage.js
@@ -1,37 +1,41 @@
-var notes = sessionStorage.getItem("notes");
-if (!notes) {
-    sessionStorage.setItem("notes", JSON.stringify([]));
-    notes = sessionStorage.getItem("notes");
-}
-notes = JSON.parse(notes);
+var data = sessionStorage.getItem("notes");
+var notes = data ? JSON.parse(data) : [];
 
-function compareNotes(s1, s2) {
-    return s1.title < s2.title;
+function compareNotesByFinished(s1, s2) {
+    return s1.erledigtbis > s2.erledigtbis;
+}
+
+function compareNotesByCreated(s1, s2) {
+    return s1.created < s2.created;
+}
+
+function compareNotesByImportance(s1, s2) {
+    return s1.rating < s2.rating;
 }
 
 function getNotes(orderBy, filterBy) {
-    if(filterBy){
-        return notes.filter(note => note.status === "closed");
-    }
-    return notes;
-}
+    var data = sessionStorage.getItem("notes");
+    var tempNotes = data ? JSON.parse(data) : [];
 
-class Note {
-    constructor(title, status) {
-        this.id = guid();
-        this.title = String(title);
-        this.status = status;
+    if (filterBy) {
+        tempNotes = tempNotes.filter(note => note.status == "erledigt");
     }
-
-    changeStatus(){
-        if(this.status === "offen"){
-            this.status = "closed";
-        }else{
-            this.status = "offen";
+    if (orderBy) {
+        switch (orderBy) {
+            case 1:
+                tempNotes.sort((a, b) => compareNotesByFinished(a, b));
+                break;
+            case 2:
+                tempNotes.sort((a, b) => compareNotesByCreated(a, b));
+                break;
+            case 3:
+                tempNotes.sort((a, b) => compareNotesByImportance(a, b));
+                break;
+            default:
         }
     }
+    return tempNotes;
 }
-
 
 function guid() {
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
@@ -44,19 +48,33 @@ function s4() {
         .substring(1);
 }
 
-function addNote(note){
-    new Note(note.title);
+function addNote(title, status, description, rating, erledigtBis) {
+    var note = JSON.parse('{"id": "' + guid() + '", "title": "' + title + '", "status": "' + status + '", "created": "' + new Date() + '", "finished": "' + null + '", "description": "' + description + '", "rating": ' + rating + ', "erledigtbis": "' + erledigtBis + '"}');
     notes.push(note);
     sessionStorage.setItem("notes", JSON.stringify(notes));
 }
-function updateNote(note){
 
+function updateNote(note) {
+    const tempToDos = notes.filter(t => t.id !== note.id);
+    notes = tempToDos;
+    notes.push(note);
+    sessionStorage.setItem("notes", JSON.stringify(notes));
 }
 
-function getNoteById(id){
-    notes.forEach(function (note) {
-        if(note.id === id){
-            return note;
-        }
-    });
+function updateNoteData(id, title, description, rating, erledigtBis) {
+    const note = getNoteById(id);
+
+    updateNote({...note, title: title, description: description})
+}
+
+function updateStatus(note) {
+    if (note.status == "offen") {
+        updateNote({...note, status: "erledigt", finished: new Date()})
+    } else {
+        updateNote({...note, status: "offen", finished: null})
+    }
+}
+
+function getNoteById(id) {
+    return notes.filter(note => note.id === id)[0];
 }
